d0d102efddbce5121195d49b236bb436d689a748
hyunmin kim <human.gim@gmail.com>
2016-08-17 14:21:42 -0600

    Create sra_to_bucket.sh

---
+++ src/sra_to_bucket.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# INPUT: SRA id
+# OUTPUT: bucket id where output FASTQ files are stored
+yalm='
+name: sra_to_fastq
+description: Run fastq_dump file to generate fastq files from SRA files
+inputParameters:
+- name: INPUT
+  defaultValue: SRR1575914 
+outputParameters:
+- name: OUTPUT
+  localCopy:
+    disk: datadisk
+    path: output.fastq.gz
+resources:
+  minimumCpuCores: 1
+  minimumRamGb: 1
+  zones:
+  - us-central1-a
+  - us-central1-b
+  - us-central1-c
+  - us-central1-f
+  disks:
+  - name: datadisk
+    type: PERSISTENT_HDD
+    sizeGb: 100
+    mountPoint: /mnt/data
+docker:
+  imageName: actor0/bam2go
+  cmd: "fastq-dump -X 100 --gzip --outdir ${OUTPUT} ${INPUT}"
+'
+
+sra_to_fastq(){
+  local input=$1
+  local output=$2
+  echo "$yalm" > ./$FUNCNAME.yalm
+gcloud alpha genomics pipelines run \
+  --pipeline-file ./$FUNCNAME.yalm \
+  --logging $output/logs \
+  --inputs INPUT=$input \
+  --outputs OUTPUT=$output
+}
+
+##sra_to_fastq SRR1575914 "gs://bg-sra"
